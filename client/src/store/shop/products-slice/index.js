05b8adbc55b3bc3efebf340fad7f09a3bffc475a
shivrawat05 <shivrawat2002@gmail.com>
2026-05-11 08:20:00 +0530

    changes

--- client/src/store/shop/products-slice/index.js
+++ client/src/store/shop/products-slice/index.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchAllFilteredProducts = createAsyncThunk(
   "shoppingProducts/fetchAllFilteredProducts",
