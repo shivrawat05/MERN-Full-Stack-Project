c4741b28bc51b32cfca7e885e6c631ce7019c2fa
shivrawat05 <shivrawat2002@gmail.com>
2026-05-24 22:03:30 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 
 const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
 
 mongoose.connect(
   "mongodb+srv://shivrawat:Rawat2002@cluster0.p2nx0ve.mongodb.net",
@@ -36,5 +37,6 @@ app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 
 app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
